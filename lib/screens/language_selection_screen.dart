3818a4d1c3dbffdce3ba54f2469aca2ee37f3a56
Priyes Ranjan <priyesranjan@gmail.com>
2026-06-06 15:06:21 +0530

    feat: expand Indian language translations and cards to include Gujarati, Bengali, Tamil, Telugu, and Kannada

--- lib/screens/language_selection_screen.dart
+++ lib/screens/language_selection_screen.dart
@@ -13,6 +13,11 @@ class LanguageSelectionScreen extends StatelessWidget {
     {'code': 'pa', 'name': 'Punjabi', 'nativeName': 'ਪੰਜਾਬੀ'},
     {'code': 'bho', 'name': 'Bhojpuri', 'nativeName': 'भोजपुरी'},
     {'code': 'mr', 'name': 'Marathi', 'nativeName': 'मराठी'},
+    {'code': 'gu', 'name': 'Gujarati', 'nativeName': 'ગુજરાતી'},
+    {'code': 'bn', 'name': 'Bengali', 'nativeName': 'বাংলা'},
+    {'code': 'ta', 'name': 'Tamil', 'nativeName': 'தமிழ்'},
+    {'code': 'te', 'name': 'Telugu', 'nativeName': 'తెలుగు'},
+    {'code': 'kn', 'name': 'Kannada', 'nativeName': 'ಕನ್ನಡ'},
   ];
 
   @override
